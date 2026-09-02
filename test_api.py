0973733d1f7a7b2bc9dfd170d8ca5ae986e0d0ad
DimChatz-data-ml <dimchatzo91@gmail.com>
2026-09-03 00:03:38 +0300

    feat: πρώτο ENTSO-E API call (day-ahead prices GR), .dockerignore, docker venv fix

---
+++ test_api.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from entsoe import EntsoePandasClient
+
+load_dotenv()
+
+api_key=os.getenv("ENTSOE_API_KEY")
+
+if not api_key:
+    print('key does not exist!')
+    exit(1)
+
+client=EntsoePandasClient(api_key=api_key)
+
+end=pd.Timestamp.now(tz="Europe/Athens")
+start=end- pd.Timedelta(days=2)
+
+try:
+    df=client.query_day_ahead_prices('GR', start=start, end=end)
+    print('everything is OK,we have data')
+    print(df.head())
+except Exception as e:
+    print(f"error fetching data {e}")
+    exit(1)
+
+
+
+
+
+
+
+
+
